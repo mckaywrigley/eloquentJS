92a4659afa9d5cf54eaa24267c5b0df9de26ac0f
Mckay <mckaywrigley@gmail.com>
2019-04-20 16:19:15 -0700

    completes countChars

--- Chapter 3 | Functions/Bean Counting/index.js
+++ Chapter 3 | Functions/Bean Counting/index.js
@@ -7,3 +7,13 @@ const countBs = str => {
   }
   return count;
 };
+
+const countChar = (str, a) => {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === a) {
+      count++;
+    }
+  }
+  return count;
+};
